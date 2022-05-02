a65a45256acdb5a4c2abccf541cbeca35585add6
TECH4ED-PC6 <TECH4ED-PC6@TECH4ED_6>
2022-05-02 14:00:40 +0800

    Add project files.

---
+++ Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,51 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorPageMovie.Data;
+using RazorPageMovie.Models;
+
+namespace RazorPageMovie.Pages.Movies
+{
+#pragma warning disable CS8618
+#pragma warning disable CS8602
+
+    public class CreateModel : PageModel
+    {
+        private readonly RazorPageMovie.Data.RazorPageMovieContext _context;
+
+        public CreateModel(RazorPageMovie.Data.RazorPageMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+#pragma warning restore CS8618
+#pragma warning restore CS8602
+
+}
---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,48 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovie.Data;
+using RazorPageMovie.Models;
+
+namespace RazorPageMovie.Pages.Movies
+{
+#pragma warning disable CS8618
+#pragma warning disable CS8604
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPageMovie.Data.RazorPageMovieContext _context;
+
+        public IndexModel(RazorPageMovie.Data.RazorPageMovieContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var movies = from m in _context.Movie
+                         select m;
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            Movie = await _context.Movie.ToListAsync();
+        }
+#pragma warning restore CS8618
+#pragma warning restore CS8604
+    }
+}
